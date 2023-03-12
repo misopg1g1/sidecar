ffc7c5c4acf43cd685b90e9b8b9c83b7a8d9250a
EdgarLuna <eluna@urchinsys.com>
2023-03-12 12:48:09 -0500

    modificando nombre de variable de entorno

--- config/default.py
+++ config/default.py
@@ -3,4 +3,4 @@ import os
 
 class AppConfigValues:
     ENCRYPTION_KEY_SECRET = os.getenv("ENCRYPTION_KEY_SECRET", "ASuJ-vtjlvAuUdFDTdeMOHoCTjlS_dipLtp6_7rQ_kw=")
-    NEST_SERVICE_URL = os.getenv("NEST_SERVICE_URL", "http://localhost:3002")
+    MICRO_SERVICE_URL = os.getenv("MICRO_SERVICE_URL", "http://localhost:3002")
