5acc13e41c50b95329c218247823804a52a200b1
EdgarLuna <eluna@urchinsys.com>
2023-05-03 08:57:50 -0500

    Fix obtener todo el body y no solo 1 chunk

--- common/middlewares_handling.py
+++ common/middlewares_handling.py
@@ -21,14 +21,18 @@ def decryptor_middleware(app: FastAPI):
     async def decrypt_body(request: Request, call_next):
         method_logger = logger.global_logger.getChild("decrypt_body")
         scope = request.scope
-        receive = await request._receive()
         send = request._send
-        if bytes_body := receive.get("body"):
-            raw_json_body = json.loads(bytes_body)
-            clean_json_body = dict(filter(lambda kv: kv[0] not in ["hash"], json.loads(bytes_body).items()))
+        # Receive the entire request body using request.stream()
+        body = b""
+        new_bytes_body = b''
+        async for chunk in request.stream():
+            body += chunk
+        if body:
+            raw_json_body = json.loads(body)
+            clean_json_body = dict(filter(lambda kv: kv[0] not in ["hash"], json.loads(body).items()))
             try:
                 fernet = Fernet(AppConfigValues.ENCRYPTION_KEY_SECRET.encode())
-                if not fernet.decrypt(raw_json_body.get('hash')).decode() == helpers.get_hash(clean_json_body):
+                if fernet.decrypt(raw_json_body.get('hash')).decode() != helpers.get_hash(clean_json_body):
                     return JSONResponse(status_code=403,
                                         content={"error": ResponseMessagesValues.NO_MATCHING_HATCH})
             except (InvalidToken, ValueError):
@@ -40,17 +44,17 @@ def decryptor_middleware(app: FastAPI):
                 return JSONResponse(status_code=400,
                                     content={"error": ResponseMessagesValues.GENERAL_ENCRYPTION_ERROR_MSG})
             new_bytes_body = json.dumps(clean_json_body).encode()
-            receive["body"] = new_bytes_body
 
         async def new_receive() -> Message:
-            return receive
+            return {"type": "http.request", "body": new_bytes_body}
 
         request = Request(scope, new_receive, send)
+
         if scope.get("path") not in ["/", "/docs", "/openapi.json", "/health/", "/health"]:
             method = request.method
             headers = request.headers
             body = await request.body()
-            body = json.loads(body or '{}') or None
+            body = json.loads(body or "{}") or None
             query_params = request.query_params
             micro_url = f"{AppConfigValues.MICRO_SERVICE_URL}{scope.get('path')}"
             try:
@@ -59,7 +63,8 @@ def decryptor_middleware(app: FastAPI):
                                                                headers=dict(headers.items()),
                                                                params=tuple(query_params.items()),
                                                                json=body)
-                return JSONResponse(status_code=response.status_code, content=response.json() if response.content else None)
+                return JSONResponse(status_code=response.status_code,
+                                    content=response.json() if response.content else None)
             except:
                 method_logger.error(traceback.format_exc())
                 return JSONResponse(status_code=503,
